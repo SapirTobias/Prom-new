863901dcc6f74e38e4b748698cc6f0ee3236c567
Sapir Tobias <sapirtobias1@gmail.com>
2026-03-21 16:51:30 +0200

    DEFININING GRIID_SIZE

--- create_patterns.py
+++ create_patterns.py
@@ -6,18 +6,17 @@ import cv2 as cv
 import PIL
 from numpy.f2py.auxfuncs import throw_error
 
-grid_width = 4
-grid_height = 4
+GRID_SIZE = 512
+GRID_WIDTH = GRID_SIZE
+GRID_HEIGHT = GRID_SIZE
 
-grid = np.arange(grid_width * grid_height, dtype = int)
-binary_grid = np.array([format(x, 'b') for x in grid], dtype=int).reshape(grid_height, grid_width)
-
-print(binary_grid)
+grid = np.arange(GRID_WIDTH * GRID_HEIGHT, dtype = int)
+binary_grid = np.array([format(x, 'b') for x in grid], dtype=int).reshape(GRID_HEIGHT, GRID_WIDTH)
 
 # Each value in the grid is log2(grid_height * grid_width) bits long,
 # And each bit determines the value of the pixel in a single pattern,
 # So this is the number of patterns we need
-num_patterns = np.log2(grid_height * grid_width)
+num_patterns = np.log2(GRID_HEIGHT * GRID_WIDTH)
 if int(num_patterns) != num_patterns:
     throw_error("grid size must ensure the number of patterns is an integer")
 patterns = []
@@ -28,8 +27,8 @@ for i in range(num_patterns):
     pattern = (binary_grid >> i) & 1
     patterns.append(pattern)
 
-patterns = np.array(patterns)
-print(patterns)
+
+
 
 
 
