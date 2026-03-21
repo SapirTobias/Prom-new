43a51eed25be475c5b23806329f637968d936bc3
Gali shalom <shalom.gali.47@gmail.com>
2026-03-21 18:57:59 +0200

    camera setup, trying to take a frame every 10 secinds

--- camera_settings.py
+++ camera_settings.py
@@ -1,24 +1,53 @@
 import numpy as np
 import cv2
 import time
+import create_patterns
+FRAME_TIME = 3
+FRAME_COUNT = create_patterns.num_patterns
+GRID_SIZE = create_patterns.GRID_SIZE
 
-FRAME_TIME = 10
-
-
+frames = []
 cap = cv2.VideoCapture(0) #defines the camera to use
 
 last_cap_time = 0
-
+frame_cnt = 0
 while True:
-     ret, frame = cap.read()
-     frame_64 = cv2.resize(frame, (512, 512))
+
+     ret, frame = cap.read() #read the image
+
+     frame_64 = cv2.resize(frame, (GRID_SIZE, GRID_SIZE)) #resize
+
+     current_time = time.time()
+     if current_time - last_cap_time > FRAME_TIME:   #if FRAME_TIME seconds passed, update the last time and add the frame to frames
+         last_cap_time = current_time
+         gray = cv2.cvtColor(frame_64, cv2.COLOR_BGR2GRAY)
+         frames.append(gray)
+         print(f"Captured frame #{len(frames)}")
+         frame_cnt += 1
+
      cv2.imshow('frame',frame_64)
 
      if cv2.waitKey(1) & 0xFF == ord('q'):
          break
 
+
+     if frame_cnt == FRAME_COUNT:
+        break
+
+# print all of the frames
+#for i, frame in enumerate(frames):
+#     cv2.imshow("Frames Playback", frame)
+
+#     print(f"Showing frame {i + 1}")
+
+ #    if cv2.waitKey(2000) & 0xFF == 27:
+ #        break
+
+
 cap.release()
 cv2.destroyAllWindows()
+
+
 
 
 
